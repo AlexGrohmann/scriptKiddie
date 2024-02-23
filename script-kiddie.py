b05fb2cddcb61ec65e7101276eb7ada22308a26f
AlexGrohmann <alex.grohmann@googlemail.com>
2024-02-23 21:05:54 +0100

    add ideas

--- script-kiddie.py
+++ script-kiddie.py
@@ -4,5 +4,15 @@ import os
 ip = input("Enter ip:")
 # output IP
 print("Added IP is: " + ip)
-# test 
+# test
 os.system("dir")
+
+# nmap
+#   port 22 open
+#       start simple bruteforce
+#           open connection and execute reverse shell
+# dirbuster
+#   read html and search for inputs
+#   cross-site-scripting
+#   sql injection
+#   look for usernames
