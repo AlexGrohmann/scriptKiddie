7b4722cd96a4168ca133cd83ed7c17351ba65a19
AlexGrohmann <alex.grohmann@googlemail.com>
2024-02-27 21:37:02 +0100

    use nmap to scan open ports

--- script-kiddie.py
+++ script-kiddie.py
@@ -3,14 +3,17 @@ import subprocess
 
 # read IP
 ip = input("Enter ip:")
+# DEBUG ONLY
+ip = "192.168.178.99"
 # output IP
 print("Added IP is: " + ip)
 # nmap
-output = subprocess.getoutput("nmap -h")
+output = subprocess.getoutput("sudo nmap -sS -O " + ip)
 # find in output
-pos1 = output.find("Nmap")
+pos1 = output.find("SERVICE")
+pos2 = output.find("Warning:")
 # slice the important news
-print(output[pos1 : pos1 + 9])
+print(output[pos1+7:pos2])
 # dirbuster
 # os.system("gobuster --help")
 
