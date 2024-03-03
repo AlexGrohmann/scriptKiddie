51a3de3e958847bfb0d942dc86ba587dee319d02
AlexGrohmann <alex.grohmann@googlemail.com>
2024-03-03 13:50:31 +0100

    refactor code into functions and create idea of backdoor pdf

--- script-kiddie.py
+++ script-kiddie.py
@@ -1,34 +1,50 @@
 import os
 import subprocess
 
-# read IP
-ip = input("Enter ip:")
-# DEBUG ONLY
-ip = "192.168.178.99"
-# output IP
-print("Added IP is: " + ip)
-# nmap
-output = subprocess.getoutput("sudo nmap -sS -O " + ip)
-# find in output
-pos1 = output.find("SERVICE")
-pos2 = output.find("Warning:")
-# slice the important news
-print(output[pos1 + 7 : pos2])
-sshOpen = output.find("open  ssh") != -1
-print("SSH is open: ", sshOpen)
-if sshOpen:
-    bruteforce = input("Bruteforce SSH Login? Y/N")
-    if bruteforce == "Y":
-        print("Lets have some fun")
-# dirbuster
-# os.system("gobuster --help")
 
-# nmap
-#   port 22 open
-#       start simple bruteforce
-#           open connection and execute reverse shell
-# dirbuster
-#   read html and search for inputs
-#   cross-site-scripting
-#   sql injection
-#   look for usernames
+def attack():
+    # read IP
+    ip = input("Enter ip:")
+    # DEBUG ONLY
+    ip = "192.168.178.99"
+    # output IP
+    print("Added IP is: " + ip)
+    # nmap
+    output = subprocess.getoutput("sudo nmap -sS -O " + ip)
+    # find in output
+    pos1 = output.find("SERVICE")
+    pos2 = output.find("Warning:")
+    # slice the important news
+    print(output[pos1 + 7 : pos2])
+    sshOpen = output.find("open  ssh") != -1
+    print("SSH is open: ", sshOpen)
+    if sshOpen:
+        bruteforce = input("Bruteforce SSH Login? Y/N")
+        if bruteforce == "Y":
+            print("Lets have some fun")
+            # TODO think about Wordlist, rockyou.txt is overkill
+            # use cewl
+    # dirbuster
+    # os.system("gobuster --help")
+
+    # nmap
+    #   port 22 open
+    #       start simple bruteforce
+    #           open connection and execute reverse shell
+    # dirbuster
+    #   read html and search for inputs
+    #   cross-site-scripting
+    #   sql injection
+    #   look for usernames
+
+
+def backdoorPDF():
+    print("meow")
+
+
+print("What do you want to do?")
+i = input("\t[1]: attack, \n\t[2]: backdoor pdf creation\n")
+if int(i) == 1:
+    attack()
+if int(i) == 2:
+    backdoorPDF()
