d85170655dc13dea6d2bbd73bbe37a3b3037c18c
AlexGrohmann <alex.grohmann@googlemail.com>
2024-04-09 22:03:20 +0200

    add os

--- tools/nmap.py
+++ tools/nmap.py
@@ -2,10 +2,13 @@
 # do not ping -PN
 # lot of stuff -A
 # -p- all ports
+import os
+
+
 def nmap():
     ip = input("enter ip \n")
     params = ""
     os_detection = input("os detection (Y/N)")
     if os_detection == "Y":
         params = params + " -O"
-    print("nmap" + params + " " + ip)
+    os.system("nmap" + params + " " + ip)
