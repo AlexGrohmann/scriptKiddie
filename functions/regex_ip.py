cc9f4bda24afb4fca43f09915504aef6a2d76860
AlexGrohmann <alex.grohmann@googlemail.com>
2024-04-20 18:42:31 +0200

    try colors

--- functions/regex_ip.py
+++ functions/regex_ip.py
@@ -1,11 +1,25 @@
 import re
 
 
+# Class of different styles
+class style:
+    BLACK = "\033[30m"
+    RED = "\033[31m"
+    GREEN = "\033[32m"
+    YELLOW = "\033[33m"
+    BLUE = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN = "\033[36m"
+    WHITE = "\033[37m"
+    UNDERLINE = "\033[4m"
+    RESET = "\033[0m"
+
+
 def get_ip():
     ip = input("please add ip: \n")
     if re.match("^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$", ip):
         return ip
     else:
-        print("please insert valid IP\n")
+        print(style.RED, "please insert valid IP\n", style.RESET)
         get_ip()
     return ""
