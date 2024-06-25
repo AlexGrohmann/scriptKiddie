5b36ed1ca09ae33dfca14b66848c21adf25f068a
AlexGrohmann <alex.grohmann@googlemail.com>
2024-06-25 21:46:07 +0200

    ignore error

--- script-kiddie.py
+++ script-kiddie.py
@@ -134,7 +134,7 @@ def crack():
         else:
             print("Hash type not found.")
     else:
-        print("Error running name-that-hash:", process.stderr.read().decode().strip())
+        print("Error running name-that-hash:", process.stderr.read().decode().strip()) # type: ignore
 
 
 def gobuster():
